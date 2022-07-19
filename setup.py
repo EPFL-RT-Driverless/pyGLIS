dc99680881afb6a6ad761740727f0829a38430a8
tudoroancea <oancea.tudor@icloud.com>
2022-07-19 17:19:52 +0000

    updated .gitpod.yml

--- setup.py
+++ setup.py
@@ -8,7 +8,11 @@ setup(
     author="Tudor Oancea",
     license="MIT",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 1 - Planning",
+        # "Development Status :: 2 - Pre-Alpha",
+        # "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
