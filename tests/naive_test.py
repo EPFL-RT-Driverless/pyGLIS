cff460806cbb755d9cd7d387a6904b5847bb4912
Ted <oancea.tudor@icloud.com>
2022-10-09 17:46:15 +0200

    :sparkles: simplified interface and fixed bugs
    Fixed bugs: scaling problems in data dumping

--- tests/naive_test.py
+++ tests/naive_test.py
@@ -2,20 +2,24 @@ import numpy as np
 
 from pyGLIS import *
 
-glis_instance = GLIS(
-    nvar=1,
-    f=lambda x: (1 + x * np.sin(2 * x) * np.cos(3 * x) / (1 + x**2)) ** 2
-    + x**2 / 12
-    + x / 10,
-    lb=-3.0,
-    ub=3.0,
-    alpha=1.0,
-    delta=0.5,
-    maxevals=140,
-    verbose=True,
-    load_previous_results=True,
-    # load_previous_results=False,
-)
-sol = glis_instance.run()
 
-print(sol["xopt"])
+def main():
+    glis_instance = GLIS(
+        nvar=1,
+        f=lambda x: (1 + x * np.sin(2 * x) * np.cos(3 * x) / (1 + x**2)) ** 2
+        + x**2 / 12
+        + x / 10,
+        lb=-3.0,
+        ub=3.0,
+        alpha=1.0,
+        delta=0.5,
+        maxevals=140,
+        verbose=True,
+        # load_previous_results=True,
+    )
+    sol = glis_instance.run()
+    print(sol["xopt"])
+
+
+if __name__ == "__main__":
+    main()
